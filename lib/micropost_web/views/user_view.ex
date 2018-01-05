7fe7f54abca8ba1f20f00413fe13e00189e22159
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-01-05 21:14:41 +0900

    add gravatar

--- lib/micropost_web/views/user_view.ex
+++ lib/micropost_web/views/user_view.ex
@@ -4,4 +4,10 @@ defmodule MicropostWeb.UserView do
   def page_title(%{private: %{phoenix_action: :new}}), do: "Sign up"
   def page_title(%{assigns: %{user: user}}), do: user.name
   def page_title(_), do: nil
+
+  def gravatar_for(user) do
+    gravatar_id = :crypto.hash(:md5, user.email) |> Base.encode16(case: :lower)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    img_tag(gravatar_url)
+  end
 end
