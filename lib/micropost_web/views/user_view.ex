8814638873e0e340e02a2865ef5b252d57ffd6ed
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-01-06 09:18:30 +0900

    add page title when sign up failed

--- lib/micropost_web/views/user_view.ex
+++ lib/micropost_web/views/user_view.ex
@@ -2,6 +2,7 @@ defmodule MicropostWeb.UserView do
   use MicropostWeb, :view
 
   def page_title(%{private: %{phoenix_action: :new}}), do: "Sign up"
+  def page_title(%{private: %{phoenix_action: :create}}), do: "Sign up"
   def page_title(%{assigns: %{user: user}}), do: user.name
   def page_title(_), do: nil
 
